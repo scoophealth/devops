0e55bce0bece4d5f5725283fd9202b72dde404df
Raymond Rusk <rusk.raymond@gmail.com>
2014-10-31 15:32:14 -0700

    Simplify mysql dumpfile name

--- Scripts/mysql_dump.sh
+++ Scripts/mysql_dump.sh
@@ -3,7 +3,7 @@
 # stop Oscar access to database
 sudo /etc/init.d/tomcat6 stop
 #
-DATE=`date +%Y%m%d-%H:%M:%S`
+DATE=`date +%Y%m%d_%H%M%S`
 echo "Enter mysql admin password..."
 mysqldump -u root -p --databases oscar_12_1 > /tmp/oscar_12_1-$DATE.sql
 #
