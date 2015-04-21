4cdb53f2e936304d2594c3966c4f8640806aadcf
Raymond Rusk <rusk.raymond@gmail.com>
2015-04-21 09:26:32 -0700

    Removed --databases option so database can be loaded with a different name

--- Scripts/mysql_dump.sh
+++ Scripts/mysql_dump.sh
@@ -5,7 +5,7 @@ sudo /etc/init.d/tomcat6 stop
 #
 DATE=`date +%Y%m%d_%H%M%S`
 echo "Enter mysql admin password..."
-mysqldump -u root -p --databases oscar_12_1 > /tmp/oscar_12_1-$DATE.sql
+mysqldump -u root -p oscar_12_1 > /tmp/oscar_12_1-$DATE.sql
 #
 # restart Oscar
 sudo /etc/init.d/tomcat6 start
